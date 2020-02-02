470bb822ec3c439f88703ddbef7e1d50658afe9e
gv4383 <gregory.vargas18@gmail.com>
2020-02-01 18:09:44 -0600

    Added linting

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
-import express, {
-  Application,
-  NextFunction,
-  Request,
-  Response,
-} from 'express';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => res.send('Hello World'));
+app.get('/', (req: Request, res: Response) => res.send('Hello World'));
 
 app.listen(5000, () => console.log('Listening on port 5000'));
