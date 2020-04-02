d4a710ff066ef3f565cd3e0456ca4dc09e4368ce
Greg Vargas <greg.vargas@silvercar.com>
2020-04-02 00:13:42 -0500

    Enabled cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import { router } from './routes';
 
 const app: Application = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', router);
 
