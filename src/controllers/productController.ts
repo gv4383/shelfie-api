f79d45c2187725596810e2988d8e1641cdb2fd74
Greg Vargas <greg.vargas@silvercar.com>
2020-04-16 20:32:39 -0500

    Initialize id and increment when new products are added

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,6 +4,8 @@ import { inventory } from './inventoryController';
 import { hasTwoDecimals } from '../utils/validators';
 import { Product } from '../types/products';
 
+let count = 2;
+
 export const getProduct = (req: Request, res: Response): void => {
   const { id } = req.params;
   const productIndex = inventory.findIndex((product: Product) => product.id === parseInt(id));
@@ -17,7 +19,12 @@ export const getProduct = (req: Request, res: Response): void => {
 
 export const addProduct = (req: Request, res: Response): void => {
   if (hasTwoDecimals(req.body.price.toString())) {
-    inventory.push(req.body);
+    const newProduct = {
+      id: count,
+      ...req.body,
+    };
+    inventory.push(newProduct);
+    count += 1;
     res.status(204).send('No Content');
   } else {
     res.status(400).send('Bad Request. Service only accepts a price with 2 decimals.');
