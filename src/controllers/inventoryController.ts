9ddb695d5775447ab5c59339334ef288874716a9
gv4383 <gregory.vargas18@gmail.com>
2021-02-20 14:32:42 -0600

    Add image_url attribute to product object

--- src/controllers/inventoryController.ts
+++ src/controllers/inventoryController.ts
@@ -6,11 +6,15 @@ import { Product } from '../types/products';
 export let inventory: Product[] = [
   {
     id: 0,
+    image_url:
+      'https://images.unsplash.com/photo-1517649357390-d11df376d303?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2850&q=80',
     name: 'Left Boot',
     price: 1.29,
   },
   {
     id: 1,
+    image_url:
+      'https://images.unsplash.com/photo-1525383734057-57832c535c9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80',
     name: 'Green Mitten',
     price: 8.69,
   },
--- src/types/products.ts
+++ src/types/products.ts
@@ -1,5 +1,6 @@
 export interface Product {
   id: number;
+  image_url: string;
   name: string;
   price: number;
 }
